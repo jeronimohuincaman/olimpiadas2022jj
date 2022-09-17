c3eaad13080b3cdd72db8e9fd9645a8ff3222028
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-09-17 11:57:26 -0300

    creamos pantalla agenda

---
+++ cliente/src/routes/Agenda.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+export default function Agenda() {
+  return (
+    <div className='home-container'>
+        <div className='header-container'>
+           <NavBar/>
+        </div>
+        <div className="contenido-container">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima accusantium quaerat quidem possimus error ex provident suscipit perferendis ut dicta! Excepturi nam ipsam vitae hic possimus doloribus distinctio odit corrupti, voluptate dolore maiores obcaecati commodi repudiandae iure tempore itaque nemo voluptatibus debitis ad eius. Quas dicta quasi, corrupti nobis optio distinctio fugit consequuntur quo perspiciatis a unde eaque, minus quaerat tempora odit at, et possimus. Illo minima, et esse non unde doloremque, rerum quasi placeat quo saepe distinctio vel asperiores labore cupiditate id, exercitationem provident porro? Vel est ut, enim, necessitatibus eveniet error, veniam commodi molestias architecto fuga quos delectus.</p>
+        </div>
+        <div className="footer-container">
+            aca iria el footer
+        </div>
+        </div>
+  )
+}
