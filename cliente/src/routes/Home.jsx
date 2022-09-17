61ca73c74bc20d462f7079f4556789ee63f72a7e
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-09-17 11:56:34 -0300

    Creamos pantalla Home

---
+++ cliente/src/routes/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import MapaDisplay from '../components/MapaDisplay'
+import NavBar from '../components/NavBar'
+import './styles/Home.css'
+
+export default function Home() {
+  return (
+    <div className='home-container'>
+        <div className='header-container'>
+            <NavBar/>
+        </div>
+        <div className="contenido-container">
+            <div className="mapa-museo">
+                <MapaDisplay/>
+            </div>
+        </div>
+        <div className="footer-container">
+            aca iria el footer
+        </div>
+    </div>
+  )
+}
