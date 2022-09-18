ee2225903af1066d5c9759b25d86428348056cae
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-09-17 21:40:52 -0300

    body parser, rutas

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ const app = express()
 //importar conexion mongoDB
 const archivoBD = require('./conexion')
 
-//importacion del archivo de rutas y modelo obras
+//importacion del archivo de rutas y modelos obras y eventos
 const rutaObra = require('./rutas/obra')
+const rutaEvento = require('./rutas/evento')
 
+//importar body parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:'true'}))
+
+//usamos las rutas
 app.use('/api/obra', rutaObra)
+app.use('/api/evento', rutaEvento)
 
 app.get('/',(req,res) => {
     res.end('bienvenidos al servidor backend Node.js. Corriendo...')
