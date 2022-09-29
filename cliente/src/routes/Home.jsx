c147933f028881acf9b318311d571c0370332a6b
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-09-29 15:26:29 -0300

    reacciones

--- cliente/src/routes/Home.jsx
+++ cliente/src/routes/Home.jsx
@@ -14,10 +14,17 @@ export default  function Home() {
         </div>
         <div className="contenido">
             <div className="mapa-museo">
+              <h1>Nuestro museo</h1>
                 <MapaDisplay/>
             </div>
 
             {/* <ListaObras/> */}
+
+            <div className="reacciones">
+              <button className="btn btn-success"> Conforme :)</button>
+              <button className="btn btn-warning"> Regular :|</button>
+              <button className="btn btn-danger"> Decepcionado :(</button>
+            </div>
         </div>
         <div className="footer">
            <Footer/>
