94aa07113fe8b5bffcfa33e994c4bfe30774c4af
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-09-29 15:26:10 -0300

    footer info

--- cliente/src/components/Footer.jsx
+++ cliente/src/components/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import './style/Footer.css'
 
 export const Footer = () => {
   return (
     
-    <div>
+    <div className='container'>
       
-        <h5>@JJmuseums</h5>
+        <h5>
+          @JJmuseums
+        </h5>
+          <p className='autor1'> Jazmin Almendra</p>
+          <p className='autor2'>Jerónimo E Huincamán </p>
+          <p className='escuela'>EPET N°20, Neuquén Cap.</p>
     </div>
   )
 
