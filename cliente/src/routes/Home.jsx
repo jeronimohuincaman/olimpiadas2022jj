785ade2176ab96dfb9a59baf2afdf1682ba32fe2
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-09-28 23:14:32 -0300

    home css

--- cliente/src/routes/Home.jsx
+++ cliente/src/routes/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Footer } from '../components/Footer'
+// import ListaObras from '../components/ListaObras'
 import MapaDisplay from '../components/MapaDisplay'
 import NavBar from '../components/NavBar'
 import './styles/Home.css'
@@ -7,16 +9,18 @@ export default  function Home() {
 
   return (
     <div className='home-container'>
-        <div className='header-container'>
+        <div className='header'>
             <NavBar/>
         </div>
-        <div className="contenido-container">
+        <div className="contenido">
             <div className="mapa-museo">
                 <MapaDisplay/>
             </div>
+
+            {/* <ListaObras/> */}
         </div>
-        <div className="footer-container">
-            aca iria el footer
+        <div className="footer">
+           <Footer/>
         </div>
     </div>
   )
